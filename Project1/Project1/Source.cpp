b455e71e06bd21d5f0edc1e1a89dc1a528ce6e26
mikce_000 <mikcel@live.com>
2016-09-17 13:27:19 -0400

    add age

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main(){
 	string name;
+	int age;
 	cout << "Hello World\n";
 	cout << "What is your name? ";
 	cin >> name;
 	cout << "Your name is " << name;
+	cout << "\n\n"
+		 << "What is your age? ";
+	cin >> age;
+	cout << "Your age is " << age;
 	while (true){};
 }
